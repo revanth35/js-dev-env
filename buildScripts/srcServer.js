291ae52d58d5f37f93e9523f6f2e57a4d8dabb84
revanth <revanth@spinspire.com>
2016-12-25 12:17:45 -0500

    added HTTP call module

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(request, response) {
+  response.json([
+    {"id":1,"firstName":"Christian", "lastName":"Crawford", "email":"christian@spinspire.com" },
+    {"id":2,"firstName":"Jitesh", "lastName":"Doshi", "email":"jitesh@spinspire.com" },
+    {"id":3,"firstName":"Revanth", "lastName":"Datla", "email":"revanth@spinspire.com" }
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -10,11 +10,11 @@ describe('First test using Mocha framework and chai assertion', () => {
 });
 
 describe('index.html', () => {
-  it('should say JS Development Environment', (done) => {
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('JavaScript Development Environment');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
